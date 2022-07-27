fd183a856fa7f1336991e59dc4b1417d040fb5fe
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-07-27 13:01:36 +1000

    Add lib

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,4 @@
 // NEEDS ALLOC FOR SPECIFIC FUNCTIONALITY
 extern crate alloc;
 
-// Most of the memory->disk functions prob dont need alloc. But the memory persistent stuff do. The memory persistent stuff could also use std in some ways
 pub mod driver;
